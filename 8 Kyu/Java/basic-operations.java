0c0c7d14d1bba4f62ebe0990fc1b1c4ba1b8a9ab
unknown <afi.firda@gmail.com>
2021-06-14 19:40:06 +0700

    more katas

---
+++ 8 Kyu/Java/basic-operations.java
@@ -0,0 +1,7 @@
+public class BasicOperations
+{
+  public static Integer basicMath(String op, int v1, int v2)
+  {
+    return op.equals("*") ? v1 * v2 : op.equals("/") ? v1 / v2 : v1 + (op.equals("+") ? v2 : -v2);
+  }
+}
